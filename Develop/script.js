92ed65da96d794bee87bb3f7e46be11e861ee7a2
Juan Carlos Marquez <JuanCarlos@MacBook-Pro.local>
2022-06-18 10:54:44 +1000

    Finisehd backgroundColor Function

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,38 @@
 
 //change the background color of the input based on time of the day.
-
 function backgroundColor(){
-    //gets current time
+
+    //gets current time and formats to 24H
     var sCurrentTime = moment().format("H");
     var iCurrentTime = parseInt (sCurrentTime);
     console.log(iCurrentTime);
 
-
     //gets string time in input element and converts to integer and 24H format.
-    var sTimeEl = $(".time-element").html();
-    console.log(sTimeEl);
-    for ( var i=0; i < sTimeEl.length; i++){
+    for ( var i=0; i < 9; i++){
+        var sTimeEl = $(".time-element").eq(i).html();
+        console.log(sTimeEl);
+        //splits string element and converts to integer
         var iTimeEl = parseInt(sTimeEl.slice(0,2));
         var mer = sTimeEl.slice(-2);
-        if (mer =="PM"){
-        iTimeEl = iTimeEl + 12;
-        }
         console.log(iTimeEl);
         console.log(mer);
+        //converts to 24H format to be able to compare with current time
+        if (mer =="PM" && (iTimeEl < 12) ){
+        iTimeEl = iTimeEl + 12;
+        }
+        
+        //compares 24H format current time with 24H formt time in input element
         var checkTime = $(".text-area");
         console.log(checkTime)
-    };
-    
-
-
-  //compares current time with time in input element
-    var inputElement = $(".text-area");
-    for (var i=0; i < inputElement.length; i++ ){
         if(iCurrentTime > iTimeEl){
-        $(".text-area").addClass("past");
-        } else if ( iCurrentTime < iTimeEl){
-        $(".text-area").addClass("future");
-        } else {
-        $(".text-area").addClass("present");
-        }          
+            $(".text-area").eq(i).addClass("past");
+            } else if ( iCurrentTime < iTimeEl){
+            $(".text-area").eq(i).addClass("future");
+            } else {
+            $(".text-area").eq(i).addClass("present");
+            }   
     };
 };
-
 backgroundColor();
 
 
